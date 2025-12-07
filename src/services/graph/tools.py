5721a51f8bfd3811fd90dfa2612f694f6be0df00
Lina-go <l.gomez1@uniandes.edu.co>
2025-12-07 17:13:07 -0500

    a

--- src/services/graph/tools.py
+++ src/services/graph/tools.py
@@ -3,7 +3,8 @@
 import plotly.graph_objects as go
 import plotly.express as px
 from typing import List, Dict, Any
-from src.config.constants import ChartType, CHART_COLORS
+from src.config.constants import ChartType
+from src.config.settings import chart_color_palette
 
 
 def generate_pie_chart(
@@ -21,7 +22,7 @@ def generate_pie_chart(
         Base64 encoded image string
     """
     if colors is None:
-        colors = CHART_COLORS
+        colors = chart_color_palette
     
     labels = [d.get("x_value", "") for d in data_points]
     values = [d.get("y_value", 0) for d in data_points]
